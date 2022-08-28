bdbe78c4558f47588dc1d41a0fac67e0feb75ca5
Doremon <www.saylipatil1603@gmail.com>
2022-08-28 23:05:24 +0530

    28 Aug 2022

---
+++ practice/dict.py
@@ -0,0 +1,23 @@
+# Creating a Dictionary
+# with each item as a Pair
+# Dict = dict([(1, 'Geeks'), (2, 'For')])
+
+'''
+1. declare input list
+2. iterate list
+3. for each element use dictionary create syntax to add key and value to dict
+
+
+
+
+'''
+
+in_list = [(1, 'Geeks'), (2, 'For')]
+result_dict = {}
+for i in range(len(in_list)):
+    print(in_list[i][0],in_list[i][1])
+    # print(elem[0],elem[1])
+    # result_dict[elem[0]] = elem[1]
+    result_dict[in_list[i][0]] = in_list[i][1]
+
+print(result_dict)
--- practice/squareKeys.py
+++ practice/squareKeys.py
@@ -3,3 +3,23 @@ Write a Python script to print a dictionary where,
 the keys are numbers between 1 and 15 (both values included) and the values are square of keys.
 
 """
+
+def to_obtain_square_of_keys_as_values():
+    dict = 0
+    for i in range(1,15):
+        dict = dict[i] * dict[i]
+        dict.append(dict)
+    return dict
+
+
+
+
+
+
+def main():
+
+    dict = to_obtain_square_of_keys_as_values()
+
+    print('this is obtained dictionary: ',dict)
+
+main()
