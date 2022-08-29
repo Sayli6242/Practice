c8a5e4f4378490555774e01ed26f1f339818876b
Doremon <www.saylipatil1603@gmail.com>
2022-08-29 23:34:13 +0530

    29 Aug 2022

---
+++ practice/electionDict.py
@@ -0,0 +1,43 @@
+"""
+Given an array of names of candidates in an election.
+ A candidate name in the array represents a vote cast to the candidate. Print the name of candidates received Max vote. 
+
+Input :  votes =["john", "john", "mackie", 
+                    "john", "john", "mackie", 
+                    "lamie", "lamie", "john",
+                    "john", "lamie", "john", 
+                    "john"];
+Output : John
+
+SPECIAL_CASE: If there is tie, print a lexicographically smaller name.
+answer steps:
+what is given
+- votes array 
+
+
+"""
+
+
+
+def main():
+    # list of votes
+    votes =["john", "john", "mackie", 
+                    "john", "john", "mackie", 
+                    "lamie", "lamie", "john",
+                    "john", "lamie", "john", 
+                    "john"]
+
+    vote_count = {}
+    # key is candidate name and value is count of votes.
+
+    # apply for loop to intrate each vote in list.
+    for vote in votes:
+        print(vote)
+        # create key , value in dictionary vote count. where key is candidate name and value is vote count.
+        existing_val = vote_count.get(vote,0)
+        vote_count[vote] = existing_val+1
+    
+    #  calculate min max in dict
+
+
+main()
--- practice/squareKeys.py
+++ practice/squareKeys.py
@@ -4,22 +4,26 @@ the keys are numbers between 1 and 15 (both values included) and the values are 
 
 """
 
+
+# initialize function to obtain square of keys as values
 def to_obtain_square_of_keys_as_values():
-    dict = 0
-    for i in range(1,15):
-        dict = dict[i] * dict[i]
-        dict.append(dict)
-    return dict
-
-
-
+    # define empty dictionary to store keys and values
+    d3 = { }
+    #  apply for loop for acces each key,value in dictionary upto given range
+    for key in range(1,15):
+        # exponent of key is assign to value to ditionary d3
+            value = key ** 2
+            # obtained value is assign to d3 as a key: value
+            d3[key] = value
+            # return dictionary d3
+    return d3
 
 
 
 def main():
 
-    dict = to_obtain_square_of_keys_as_values()
+    d3 = to_obtain_square_of_keys_as_values()
 
-    print('this is obtained dictionary: ',dict)
+    print('this is obtained dictionary: ',d3)
 
 main()
