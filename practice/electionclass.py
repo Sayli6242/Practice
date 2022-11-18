baad3c297705e02d3868282e3d2b835eb28f3f6e
Doremon <www.saylipatil1603@gmail.com>
2022-11-18 16:06:01 +0530

    Flask

---
+++ practice/electionclass.py
@@ -0,0 +1,36 @@
+class election:
+    def __init__(self):
+        self.__candidate_lst =[ ]
+        # self.voters_lst =[ ]
+        self.__votes = [ ]
+
+
+    # constructor
+
+    # properties
+
+    # methods
+    def candidate_register(self):
+        for i in range(4):
+            x = input('enter candidate name')
+            self.__candidate_lst.append(x)
+            print(self.__candidate_lst)
+
+    def collect_votes(self):
+        for i in range(10):
+            y = input('enter votes for candidates')
+            self.__votes.append(y)
+            print(self.__votes)
+    
+    def calculate_votes():
+        pass
+
+
+e = election()
+
+# e = election()
+
+print(e.candidate_register())
+
+
+
