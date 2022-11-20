cd1e7967248e497f9bb6f6e68ac0dbdb31a07d4d
Doremon <www.saylipatil1603@gmail.com>
2022-11-20 16:00:35 +0530

    classes

--- practice/class2.py
+++ practice/class2.py
@@ -1,26 +1,39 @@
-
 class student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def get_grade(self):
+        return self.grade
+
+
+class Course:
     def __init__(self, name, max_students):
         self.name = name
         self.max_students = max_students
         self.students = []
+        self.is_active = False
 
     def add_student(self, student):
         if len(self.students) < self.max_students:
             self.students.append(student)
             return True
         return False
-    
+
     def get_average_grade(self):
         value = 0
         for student in self.students:
+            value += student.get_grade()
+        return value / len(self.students)
 
-            value +=student    
-s1 = Student('Tim', 18,57)
-s2 = Student('Bill', 19,75)
-s3 = student('jill',19,6500)
-course = Course(;='student')
 
-course =Course('Science', 2)
-course.add.student(s1)
-course.add.student(s2)
+s1 = student("Tim", 18, 57)
+s2 = student("Bill", 19, 75)
+s3 = student("jill", 19, 65)
+
+Course = Course("Science", 2)
+Course.add_student(s1)
+Course.add_student(s2)
+print(Course.add_student(s3))
+print(Course.get_average_grade())
