6cbcece2e3cc784b65e1f0060b93723aa57c747d
Doremon <www.saylipatil1603@gmail.com>
2022-11-22 13:08:53 +0530

    ...

--- practice/tempCodeRunnerFile.py
+++ practice/tempCodeRunnerFile.py
@@ -1,27 +1,2 @@
-
-  # if player_choice == computer_choice:
-  #   print('Match is DRAW ')
-
-
-  # elif player_choice == 'Scissors':
-  #   if computer_choice == 'Paper':
-  #     print('player is winner!,Scissors beat Paper')
-  #   else:
-  #     print('player is lost!, Rock beat Scissors')
-
-
-  # elif player_choice == 'Rock':
-  #   if computer_choice == 'Scissors':
-  #     print('player is winner!, Rock beat Scissors')
-  #   else:
-  #     print('player is lost!, Paper covers Rock')
-
-
-  # elif player_choice == 'Paper':
-  #   if computer_choice == 'Rock':
-  #     print('player is winner!, Paper covers Rock')
-  #   else:
-  #     print('player is lost, Scissors beat Paper')
-
-
-  # return result
+  # def __init__(self, name, age, color):
+    #     self.color = color
