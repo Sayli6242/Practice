c55d0a0614928321790a2fb37a4adedd37f75ee5
Doremon <www.saylipatil1603@gmail.com>
2022-11-23 14:27:25 +0530

    23Nov2022

--- practice/sal_example_func.py
+++ practice/sal_example_func.py
@@ -1,24 +1,35 @@
+"""
+# example for program pattern builder.
+
+
+"""
+
+
 def processSalary(name, role, salary):
-    print(name,role,salary)
+    print(name, role, salary)
+
 
 def getSalary():
     return 10
 
+
 def getName():
     return "Ace"
+
 
 def getROle():
     return "dev"
 
+
 def main():
     for i in range(100):
-        
-        emp = Employee(i,i,i)
+
+        emp = Employee(i, i, i)
         print(emp.processSal())
 
 
 class Employee:
-    def __init__(self,name,role,salary,bonus) -> None:
+    def __init__(self, name, role, salary, bonus) -> None:
         self.name = name
         self.role = role
         self.salary = salary
@@ -26,5 +37,6 @@ class Employee:
 
     def processSal(self):
         return self.salary + self.bonus
+
 
 main()
