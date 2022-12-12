b94ef873969774be8013f700a6cb498b589b1e50
Doremon <www.saylipatil1603@gmail.com>
2022-12-12 21:35:15 +0530

    12Dec2022

--- Classes/AbstractFactory2.py
+++ Classes/AbstractFactory2.py
@@ -14,6 +14,7 @@ class lightBgColor(BgColor):
 class darkBgColor(BgColor):
 
     pass
+    
 
 
 class Text:
@@ -43,4 +44,4 @@ class DarkFactory(ThemeFactory):
 def client(ThemeFactory):
     # creatw text
     # create bg
-    main()
+    pass
