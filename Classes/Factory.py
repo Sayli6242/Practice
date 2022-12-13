54631c1fdce765d6e1bc2e5b5eeb6868974e8ba6
Doremon <www.saylipatil1603@gmail.com>
2022-12-13 22:36:10 +0530

    13Dec2022

---
+++ Classes/Factory.py
@@ -0,0 +1,39 @@
+"""
+
+"""
+
+
+class mario:
+    pass
+
+
+class cape_mario:
+    pass
+
+
+class Fire_mario:
+    pass
+
+
+class mario_maker:
+    pass
+
+
+class fire_maker:
+    pass
+
+
+class cape_maker:
+    pass
+
+
+class Game_manager:
+    pass
+
+
+def main():
+
+    mario()
+    mario_maker()
+
+    main()
