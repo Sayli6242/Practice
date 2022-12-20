d384c61ca972aca28ac907bdf57408668294a82a
Doremon <www.saylipatil1603@gmail.com>
2022-12-20 21:18:58 +0530

    20Dec2022

---
+++ Classes/singleton.py
@@ -0,0 +1,20 @@
+"""
+Class has only one instance.
+
+"""
+
+
+class singleton:
+    def __init__(self):
+        pass
+
+    def getInstace():
+        pass
+
+
+def main():
+    obj = singleton()
+    obj = singleton.getInstance()
+
+
+main()
