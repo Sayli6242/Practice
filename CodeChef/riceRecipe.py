3cc67f99327ab3e4018f3d5c3972302a50ad2718
Doremon <www.saylipatil1603@gmail.com>
2023-01-15 22:58:33 +0530

    15Jan2023

--- CodeChef/riceRecipe.py
+++ CodeChef/riceRecipe.py
@@ -9,5 +9,8 @@
 T = int(input())
 for t in range(T):
     N = int(input())
-    votes = input()
-    print(votes)
+    for i in range(N):
+        vote = input().split()
+        d = {}
+        d = {vote[i]: vote[i + 1] for i in range(0, len(vote))}
+    print(d)
