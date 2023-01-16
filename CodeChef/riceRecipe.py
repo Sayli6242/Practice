06ec07b3485e76309800ba84865dea6fe77c8848
Doremon <www.saylipatil1603@gmail.com>
2023-01-16 23:47:39 +0530

    16Jan2023

--- CodeChef/riceRecipe.py
+++ CodeChef/riceRecipe.py
@@ -1,5 +1,18 @@
 """
+Did you know that there are over 40,000 varieties of Rice in the world ? There are so many dishes that can be prepared with Rice too. 
+A famous chef from Mumbai, Tid Gusto prepared a new dish and named it 'Tid Rice'. He posted the recipe in his newly designed blog for community voting, where a user can plus (+) or minus (-) the recipe. 
+The final score is just the sum of all votes, where (+) and (-) are treated as +1 and -1 respectively. But, being just a chef ( and not a codechef ) he forgot to take care of multiple votes by the same user.
 
+A user might have voted multiple times and Tid is worried that the final score shown is not the correct one. Luckily, he found the user logs, which had all the N votes in the order they arrived. 
+Remember that, if a user votes more than once, the user's previous vote is first nullified before the latest vote is counted ( see explanation for more clarity ). Given these records in order ( and being a codechef yourself :) ), calculate the correct final score.
+
+Input
+First line contains T ( number of testcases, around 20 ). T cases follow. Each test case starts with N ( total number of votes, 1 <= N <= 100 ). 
+Each of the next N lines is of the form "userid vote" ( quotes for clarity only ), where userid is a non-empty string of lower-case alphabets ( 'a' - 'z' ) not more than 20 in length and vote is either a + or - . 
+See the sample cases below, for more clarity.
+
+Output
+For each test case, output the correct final score in a new line
 
 
 
@@ -8,9 +21,15 @@
 
 T = int(input())
 for t in range(T):
-    N = int(input())
-    for i in range(N):
-        vote = input().split()
-        d = {}
-        d = {vote[i]: vote[i + 1] for i in range(0, len(vote))}
-    print(d)
+    n = int(input())
+    d = {}
+    for i in range(n):
+        v_id, vote = input().split()
+        d[v_id] = vote
+    votes = 0
+    for i in d.values():
+        if i == "+":
+            votes += 1
+        else:
+            votes -= 1
+    print(votes)
---
+++ CodeChef/toristDict.py
@@ -0,0 +1,39 @@
+"""
+A tourist is visiting Byteland. The tourist knows English very well. The language of Byteland is rather different from English. To be exact it differs in following points:
+
+Bytelandian alphabet has the same letters as English one, but possibly different in meaning. Like 'A' in Bytelandian may be 'M' in English. However this does not mean that 'M' in Bytelandian must be 'A' in English.
+ More formally, Bytelindian alphabet is a permutation of English alphabet. It will be given to you and could be any possible permutation. 
+Don't assume any other condition.
+People of Byteland don't like to use invisible character for separating words. Hence instead of space (' ') they use underscore ('_'). Other punctuation symbols, like '?', '!' remain the same as in English.
+The tourist is carrying "The dummies guide to Bytelandian", for translation. The book is serving his purpose nicely. But he is addicted to sharing on BaceFook, and shares his numerous conversations in Byteland on it. 
+The conversations are rather long, and it is quite tedious to translate for his English friends, so he asks you to help him by writing a program to do the same.
+
+Input
+The first line of the input contains an integer T, denoting the length of the conversation, and the string M, denoting the English translation of Bytelandian string "abcdefghijklmnopqrstuvwxyz". 
+T and M are separated by exactly one space. Then T lines follow, each containing a Bytelandian sentence S which you should translate into English. See constraints for details.
+
+Output
+For each of the sentence in the input, output its English translation on a separate line. Replace each underscores ('_') with a space (' ') in the output. Each punctuation symbol (see below) should remain the same. 
+Note that the uppercase letters in Bytelandian remain uppercase in English, and lowercase letters remain lowercase. See the example and its explanation for clarity.
+
+
+"""
+
+
+T, M = input().split()
+a = "abcdefghijklmnopqrstuvwxyz"
+A = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for i in range(int(t)):
+    x = input()
+    for j in x:
+        if j in A:
+            X = A.index(j)
+            print(M[X].upper(), end="")
+        elif j in a:
+            X = a.index(j)
+            print(M[X].lower(), end="")
+        elif j == "_":
+            print(" ", end="")
+        else:
+            print(j, end="")
+    print()
