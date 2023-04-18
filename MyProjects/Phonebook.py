9319b065bb81b6bfe20d330b7b4247fb38f895c3
Doremon <www.saylipatil1603@gmail.com>
2023-04-18 22:33:45 +0530

    18 apr 2023

--- MyProjects/Phonebook.py
+++ MyProjects/Phonebook.py
@@ -1,24 +1,69 @@
 """
-1) get how many number of contact are added.
-2) ask user for username
-3) ask user for contacts
-4) ask user for EmailId
-5) after all steps display massage of 'contact added'.
+1) get mode (add, search)
+2) if add,
+    - get contact details
+    - store into file/database
+3) if search,
+    - ask for details to be search
+    - search by detail
+        - if found ,show details
+        - else, show not found.
 
 """
+
+
+
 import click
 
 @click.command()
+@click.option('--add', default = 1, help ='To add contact' )
+
 @click.option('--contact',prompt="contact",default=1,help='enter contact,you want to add')
 @click.option('--name',prompt='username',help ='enter username of contact')
 @click.option('--email',prompt='Email',help ='enter Email address of contact')
-@click.option('--contact',prompt="contact",default=1,help='enter contact,you want to add')
-def phonebook(name,contact,email):
+
+def phonebook(name,contact):
     for i in range(contact):
-        click.echo(f'username : {name}')
-        click.echo(f'contact : {contact}')
-        click.echo(f'Email : {email}')
+        print(f'username : {name}')
+        print(f'contact : {contact}')
+       
+
+def add(names,numbers):
+    
+    for i in range(num):
+        name =  input('name: ')
+        number = input('phone number')
+        names.append(name)
+        numbers.append(number)
+        
+    print('\nName \t \t \t phone number \n')
+
+
+def search(num,names,numbers):
+    for i in range(num):
+        print('{}\t\t\t{}'.format(names[i], numbers[i]))
+
+    search_term = input('\Enter search Term: ')
+    print('search result')
+
+
+    if search_term in names:
+        index = names.index(search_term)
+        number = numbers[index]
+        print('Name {}, phone number: {},Email: {}'.format(search_term, number))
+    
+    else:
+        print('Name not found')
+    print('search')
+
 
 
 if __name__ == '__main__':
+    names = [ ]
+    numbers = [ ]
+    num = 3 
+    
+    
+    add(names,numbers,num)
+    search(num,names,numbers)
     phonebook()
