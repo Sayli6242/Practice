29415034705ecaea4667889415cd6faa8debffbd
Sayli Patil <www.saylipatil1603@gmail.com>
2023-06-13 23:00:38 +0530

    13 june 2023

---
+++ MyProjects/mp5/database.py
@@ -0,0 +1,26 @@
+"""
+ in this program we have to use two database(sqlite and postgreSQL).
+ 
+ for that we use Factory Design Pattern 
+    - there is interface of class Repository_Database
+        - Sqlite_Repository and postgreSQL_Repository is their concrete classes
+    - in Repository_Database we define our operations that we have to required 
+        - INSERT,UPDATE and DELETE
+
+"""
+
+class Repository_Database:
+    pass
+
+
+
+
+class Sqlite_Repository:
+    
+    pass
+
+
+
+
+class postgreSQL_Repository:
+    pass 
--- MyProjects/mp5/task.py
+++ MyProjects/mp5/task.py
@@ -11,38 +11,31 @@ This program should provide various operations such as
     - tracking status
 
 1) ask user to choose database (sqlite OR PostgreSQL)
-
+    if user choose Sqlite then. 
     - create table Repository_Sqlite if not exist to store task and their details.
-
+    if user choose PostgreSQL.
     - create table Repository_PostgreSQL if not exist to store task and their details.
-
-2) make class of Task_manager
-
-    - define their attributes 
-
-        - 
-
-
-3)
-
-4)
-
-5)
-
-
+2) define fucntion name trigger_task_management.
+    - this function act as entry point of task management app
+    - it diplay list of options to the user and allow them to choose which operation they want to perform(create_task, update_task, delete_task , retrieve_data).
+    - take user choice as input and validate to ensure choice is within range of options
+    - 
+3) 
 
 """
-
-from validationHelper import check_number_in_range
-
+import click
+from mp4helpers.validationHelper import check_number_in_range
+@click.command()
 def trigger_task_management():
     # 
-    lst = ['Create Task','Update Task','Delete Task']
+    lst = ['Create Task','Update Task','Delete Task','Retrieve Task']
 
-    for i, item in enumerate(lst, ):
-        click.echo(f"{i+1}) {item}")
+   
 
     while True:
+        for i, item in enumerate(lst, ):
+            click.echo(f"{i+1}) {item}")
+
         choice = int(input("Enter your choice: ").strip())
         if not  check_number_in_range(choice,len(lst)):
             print('choice must be an number from given choices')
@@ -52,10 +45,14 @@ def trigger_task_management():
             T.create_task()
         
         if choice == 2:
-            delete_task()
+            T.delete_task()
 
         if choice == 3:
-            update_task()
+            T.update_task()
+
+        if choice == 4:
+            T.retrieve_task()
+
 
 def create_table_if_not_exist():
     pass
@@ -81,15 +78,12 @@ class task_manager:
     def delete_task(self):
         pass
 
-
-
-
-
-
-
-
+    
+    def retrieve_task(self):
+        pass
 
 
 if __name__ == '__main__':
+    T = task_manager()
 
     trigger_task_management()
