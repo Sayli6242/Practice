03ba5223259716923ab01cdc558fc9fd031c7ba2
Sayli Patil <www.saylipatil1603@gmail.com>
2024-01-11 17:51:04 +0530

    11 jan 2024

--- FIle Handeling/linked_list.py
+++ FIle Handeling/linked_list.py
@@ -1,14 +1,26 @@
 """
 define node class
-    - value 
-    - next
+    - which contain value and address of next variable(value, next)
+    - store value of start node 
+    - then define address(next) of next node.
+    - when we create start node then address of next node is None.
+
 define class linked list
-    - start node 
-    - end node
-    - define function to add node 
-        - check node is empty or not 
-            - if yes then assign value
-            - if not then  assing next to new_value
+    - define method 
+    - initially firtst node in linked list is empty
+    - define function to add node.
+        - pass value to function
+        - then add node using node class then check if list is empty or not
+        - if list is empty then current node is first and only node in list.
+        - if linked list is not empty then
+            - add new node at end of list.
+    
+    - print linked list.
+        - printing linked list we move next node until it reaches the end.
+        - print value of current node 
+        - move to next node of linked list
+        - print if no value is found
+    
 """
 
 #define class name Node
@@ -31,28 +43,47 @@ class Linked_list:
         self.start_node = None
 
     # define function to add node 
-    def Add_Node(self, value):
+    def Add_Node(self, value): 
         # add new node using Node class
         new_node = Node(value)
         # check if Linked_list is empty or not
         
         if self.start_node == None:
             # if yes then current node is the first and only node in list
-            self.start_node = node
+            self.start_node = new_node
         # if Linked_list is not empty then
         else:
-            # then add new node to beginning 
-            new_node.next = self.start_node
-            # next node is assigned to new_node
-            self.start_node = new_node
+            # Traverse to the last node
+            current_node = self.start_node
+            while current_node.next != None:
+                current_node = current_node.next
+                # Add the new node at the end
+            current_node.next = new_node
+            # Update the end to be the new node
+            self.end = new_node
+            
 
-    def Remove_Node(self, value):
-        pass
-
+    def print_linked_list(self):
+       
+        current_node = self.start_node
+        # while printing linked list we move next node until it reaches the end.
+        while current_node != None:
+            # print value of current node 
+            print(current_node.value, end=" ")
+            # move to next node of linked list
+            current_node = current_node.next
+            # print if no value is found
+        # print("list ended")
+        print(None)
+    
 
 linked_list = Linked_list()
 linked_list.Add_Node(2)
 linked_list.Add_Node(3)
-
+linked_list.Add_Node(5)
 # [2]   [3]
-# st
+# st    2nd
+#
+
+# print linked list
+linked_list.print_linked_list()    
