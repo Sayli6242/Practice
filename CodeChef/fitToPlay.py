defb4276923f4da5204ac9121ef757f9175b9982
Sayli Patil <www.saylipatil1603@gmail.com>
2024-02-25 23:13:46 +0530

    25-02-2024

--- CodeChef/fitToPlay.py
+++ CodeChef/fitToPlay.py
@@ -1,21 +1,26 @@
 """
-
+1) 
 
 
 """
+
 T = int(input())
+
 for i in range(T):
-  n =int(input())
-    goals=list(map(int, input().split()))
-    min_goal= 0
-    max_goal=0
-    for i in range(n):
-        if goals[i] < min_goal:
-            min_goal = goals[i]
-        elif goals[i] - min_goal > max_goal:
-             max_goal = goals[i] - min_goal
-    if max_goal > 0:
-        print(max_goal)
+    
+    num_of_practice_matches = int(input())
+    
+    if num_of_practice_matches < 2:
+        print("Unfit")  
     else:
-        print("UNFIT")
-    """
+       
+        max_improvement = 0
+
+        for i in range(num_of_practice_matches):
+            
+            goals_scored = int(input())
+            max_improvement = max(max_improvement, goals_scored)
+        if max_improvement == goals_scored:
+            print("Unfit") 
+        else:
+            print("Fit")  
